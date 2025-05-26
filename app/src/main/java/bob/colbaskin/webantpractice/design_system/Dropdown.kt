ab1be36c37cf01be26ede6cb4627d2ed7ba66f02
bybuss <kukuhaha729@gmail.com>
2025-05-26 18:05:12 +0300

    added custom DropdownMenu

--- app/src/main/java/bob/colbaskin/webantpractice/design_system/Dropdown.kt
+++ app/src/main/java/bob/colbaskin/webantpractice/design_system/Dropdown.kt
@@ -1,13 +1,144 @@
 package bob.colbaskin.webantpractice.design_system
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import bob.colbaskin.webantpractice.R
+import bob.colbaskin.webantpractice.design_system.theme.CustomTheme
+import bob.colbaskin.webantpractice.design_system.theme.WebAntPracticeTheme
 
 @Composable
-fun CustomDropdownMenu() {
-    // TODO: Implement
+fun Dropdown(
+    modifier: Modifier = Modifier,
+    menuItemData: List<String>,
+    expanded: Boolean,
+    onDismissRequest: () -> Unit,
+    onItemClick: () -> Unit
+) {
+
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = onDismissRequest,
+        modifier = modifier,
+        containerColor = CustomTheme.colors.white,
+    ) {
+        menuItemData.forEach { text ->
+            DropdownItem(
+                text = text,
+                onClick = onItemClick
+            )
+        }
+    }
 }
 
 @Composable
-fun CustomDropdownItem() {
-    // TODO: Implement
+fun DropdownItem(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed = interactionSource.collectIsPressedAsState().value
+    var isEnabled = remember { mutableStateOf(true) }.value
+
+    DropdownMenuItem(
+        text = {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        },
+        onClick = onClick,
+        enabled = isEnabled,
+        modifier = modifier.background(
+            when {
+                isPressed -> CustomTheme.colors.main
+                !isEnabled -> CustomTheme.colors.grayLight
+                else -> CustomTheme.colors.white
+            }
+        ),
+        colors = MenuDefaults.itemColors(
+            textColor = when {
+                isPressed -> CustomTheme.colors.white
+                else -> CustomTheme.colors.black
+            },
+            disabledTextColor = CustomTheme.colors.gray
+        ),
+        interactionSource = interactionSource
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DropdownPreview() {
+    var expanded by remember { mutableStateOf(false) }
+
+    WebAntPracticeTheme {
+        Box(modifier = Modifier.padding(16.dp)) {
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(
+                    painter = painterResource(R.drawable.more_vert),
+                    contentDescription = stringResource(R.string.more_vert_logo_description)
+                )
+            }
+            Dropdown(
+                menuItemData = listOf(
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item",
+                    "Menu Item"
+                ),
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                onItemClick = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DropdownItemPreview() {
+    WebAntPracticeTheme {
+        DropdownItem(text = "Menu Item", onClick = {})
+    }
 }
