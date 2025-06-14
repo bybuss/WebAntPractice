94fb5750a4fdddc285fd0a0870978e5c4cac22f9
bybuss <kukuhaha729@gmail.com>
2025-06-15 01:59:32 +0300

    added: detailed photo screen

--- app/src/main/java/bob/colbaskin/webantpractice/home/data/PhotoMappers.kt
+++ app/src/main/java/bob/colbaskin/webantpractice/home/data/PhotoMappers.kt
@@ -1,10 +1,17 @@
 package bob.colbaskin.webantpractice.home.data
 
+import bob.colbaskin.webantpractice.common.utils.parseToMillis
+import bob.colbaskin.webantpractice.home.data.models.FullFileResponse
+import bob.colbaskin.webantpractice.home.data.models.FullPhotoResponse
 import bob.colbaskin.webantpractice.home.data.models.PhotoFileResponse
 import bob.colbaskin.webantpractice.home.data.models.PhotoResponse
 import bob.colbaskin.webantpractice.home.data.models.PhotosResponse
+import bob.colbaskin.webantpractice.home.data.models.UserResponse
+import bob.colbaskin.webantpractice.home.domain.models.FullFile
+import bob.colbaskin.webantpractice.home.domain.models.FullPhoto
 import bob.colbaskin.webantpractice.home.domain.models.Photo
 import bob.colbaskin.webantpractice.home.domain.models.PhotoFile
+import bob.colbaskin.webantpractice.home.domain.models.User
 
 fun PhotosResponse.toDomain(): List<Photo> {
     return this.hydraMember.map { it.toDomain()}
@@ -23,5 +30,37 @@ fun PhotoFileResponse.toDomain(): PhotoFile {
     return PhotoFile(
         id = this.id,
         path = this.path
+    )
+}
+
+fun FullPhotoResponse.toDomain(): FullPhoto {
+    return FullPhoto(
+        id = this.id,
+        file = this.file.toDomain(),
+        user = this.user.toDomain(),
+        description = this.description,
+        name = this.name,
+        new = this.new,
+        popular = this.popular,
+        dateCreate = this.dateCreate.parseToMillis(),
+        dateUpdate = this.dateUpdate.parseToMillis()
+    )
+}
+
+fun FullFileResponse.toDomain(): FullFile {
+    return FullFile(
+        id = this.id,
+        path = this.path,
+        dateCreate = this.dateCreate.parseToMillis(),
+        dateUpdate = this.dateUpdate.parseToMillis()
+    )
+}
+
+fun UserResponse.toDomain(): User {
+    return User(
+        id = this.id,
+        displayName = this.displayName,
+        dateCreate = this.dateCreate.parseToMillis(),
+        dateUpdate = this.dateUpdate.parseToMillis()
     )
 }
