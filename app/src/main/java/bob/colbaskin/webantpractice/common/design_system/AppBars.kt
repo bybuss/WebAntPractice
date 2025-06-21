5ed6593d4d86e1d432fa49fada8dde731d7b3ca1
bybuss <kukuhaha729@gmail.com>
2025-06-22 02:34:00 +0300

    fixed: top app bars

--- app/src/main/java/bob/colbaskin/webantpractice/common/design_system/AppBars.kt
+++ app/src/main/java/bob/colbaskin/webantpractice/common/design_system/AppBars.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.input.TextFieldState
